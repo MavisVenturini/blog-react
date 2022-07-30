663f8190856f7990a3b311b3f4c1f343ad2bfb4e
unknown <mvventurini9@gmail.com>
2022-07-30 03:12:52 -0300

    projeto blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Footer from './components/estaticos/footer/Footer';
+import Navbar from './components/estaticos/navBar/Navbar';
+import Login from './paginas/login/Login';
 import './App.css';
-import Home from './paginas/home/Home';
+import Home from './paginas/home/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-let nome = "Maria Vitoria";
 function App() {
   return (
-    <Home/>
-   );
- }
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
 import './Home.css';
 
-function Home () {
+function Home() {
     return (
         <>
-            <Grid container>
-                <Grid item>
-
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo' style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo' style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                    </Box>
                 </Grid>
-                <Grid item container>
-                    <Grid item>
-
-                    </Grid>
-                    <Grid item>
-
-                    </Grid>
-
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className='postagens'>
                 </Grid>
             </Grid>
-    
         </>
     );
 }
